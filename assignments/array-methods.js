151fdfbbba4a2727b7e7770ce5b3abf3ea2badad
pgambino5851 <pgambino5851@gmail.com>
2019-02-26 18:37:02 -0500

    Finished all MVP problems for array methods.js

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,21 +56,36 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+
+runners.forEach(function(currentValue){
+    fullName.push(`${currentValue.first_name} ` + `${currentValue.last_name}`)
+});
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+allCaps = runners.map(function(currentValue){
+    return currentValue.first_name.toUpperCase();
+});
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+largeShirts = runners.filter(function(currentValue){
+   if(currentValue.shirt_size.includes('L')){
+       return currentValue;
+   } 
+});
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+ticketPriceTotal = runners.reduce(function(total, currentValue){
+    return total + currentValue.donation;
+}, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
@@ -78,6 +93,49 @@ console.log(ticketPriceTotal);
 
 // Problem 1
 
+let govtWorkers = [];
+govtWorkers = runners.filter(function(currentValue){
+    if(currentValue.email.includes('.gov')){
+        return currentValue.first_name + " " + currentValue.last_name;
+    }
+});
+console.log(govtWorkers);
 // Problem 2
 
+let sortedByDonation = [];
+let bestDonors = [];
+
+sortedByDonation = runners.filter(function(currentValue){
+    if(currentValue.donation > 200){
+        return sortedByDonation.push(currentValue);
+    }
+})
+
+sortedByDonation.sort(function(a, b){
+    return a.donation - b.donation;
+});
+
+sortedByDonation.forEach(function(currentValue){
+    bestDonors.push(`${currentValue.first_name} ` + `${currentValue.last_name}, $${currentValue.donation}`);
+});
+
+console.log(bestDonors);
+
+// bestDonors = sortedByDonation.forEach(function(currentValue){
+//     bestDonors.push(`${currentValue.first_name} ` + `${currentValue.last_name}, $${currentValue.donation}`);
+// });
+
+// console.log(bestDonors);
+
+
 // Problem 3
+
+let lastNamesThatStartWithT = [];
+lastNamesThatStartWithT = runners.filter(function(currentValue){
+    if(currentValue.last_name.charAt(0) === 'T'){
+        return currentValue;
+    }
+});
+
+
+console.log(lastNamesThatStartWithT);
